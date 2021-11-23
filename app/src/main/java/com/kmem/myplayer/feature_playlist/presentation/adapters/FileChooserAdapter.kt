93b4a6fe8fcee31dbb760b0a413d9bdd7ad1e933
kmem1 <konstantinovich.aleks@mail.ru>
2021-11-23 21:07:16 +0300

    Add viewBinding to FileChooserAdapter.

--- app/src/main/java/com/kmem/myplayer/feature_playlist/presentation/adapters/FileChooserAdapter.kt
+++ app/src/main/java/com/kmem/myplayer/feature_playlist/presentation/adapters/FileChooserAdapter.kt
@@ -2,14 +2,12 @@ package com.kmem.myplayer.feature_playlist.presentation.adapters
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kmem.myplayer.R
+import com.kmem.myplayer.databinding.FileListItemBinding
+import com.kmem.myplayer.feature_playlist.domain.model.filechooser.FileModel
 import com.kmem.myplayer.feature_playlist.domain.model.filechooser.FileTreeComponent
 
 class FileChooserAdapter(private var listener: Listener? = null)
@@ -22,41 +20,43 @@ class FileChooserAdapter(private var listener: Listener? = null)
         fun onCheckboxClick(position: Int, value: Boolean)
     }
 
-    inner class ViewHolder(val fileView: LinearLayout) : RecyclerView.ViewHolder(fileView)
-
     override fun getItemCount(): Int = treeList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val fileView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.file_list_item, parent, false) as LinearLayout
-        return ViewHolder(fileView)
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.file_list_item, parent, false)
+        val binding = FileListItemBinding.bind(view)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val fileView = holder.fileView
-        val fileIcon = fileView.findViewById<View>(R.id.file_icon) as ImageView
-        val fileModel = treeList[position].model!!
-
-        if (fileModel.file.isDirectory)
-            fileIcon.setImageResource(R.drawable.baseline_folder_24)
-        else
-            fileIcon.setImageResource(R.drawable.baseline_file_24)
-
-        val fileName = fileView.findViewById<View>(R.id.file_name) as TextView
-        fileName.text = fileModel.name
-        fileView.setOnClickListener { listener?.onListItemClick(position) }
-        val checkbox = fileView.findViewById<CheckBox>(R.id.checkbox)
-        checkbox.isChecked = treeList[position].isSelected
-        checkbox.setOnClickListener { buttonView ->
-            if (buttonView is CheckBox) {
-                listener?.onCheckboxClick(position, buttonView.isChecked)
-            }
-        }
+        holder.bind(treeList[position].model!!)
     }
 
     @SuppressLint("NotifyDataSetChanged")
     fun setData(newList: List<FileTreeComponent>) {
         treeList = newList
         notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val binding: FileListItemBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(model: FileModel) {
+            if (model.file.isDirectory) {
+                binding.fileIconIv.setImageResource(R.drawable.baseline_folder_24)
+            } else {
+                binding.fileIconIv.setImageResource(R.drawable.baseline_file_24)
+            }
+
+            binding.fileNameTv.text = model.name
+            binding.root.setOnClickListener { listener?.onListItemClick(position) }
+            binding.checkbox.isChecked = treeList[position].isSelected
+            binding.checkbox.setOnClickListener { buttonView ->
+                if (buttonView is CheckBox) {
+                    listener?.onCheckboxClick(position, buttonView.isChecked)
+                }
+            }
+        }
     }
 }
